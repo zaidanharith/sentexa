407be03b0f5caf2f59663f2eec6840f0a6be0766
DzulfikarRizqi <dzulfikarrizqii@gmail.com>
2026-03-09 14:36:55 +0700

    FEAT: Menambahkan navbar di landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,56 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="bg-white text-slate-600 px-48 h-full w-full">
+      <nav className="flex items-center justify-between px-8 py-4 w-full">
+        <div className="flex items-center gap-4">
+          {/* Logo */}
+          <div className="flex items-center gap-2">
+            <Image src="/logo1.png" alt="Logo" width={100} height={100} />
+            <span className="text-2xl font-bold text-blue-600">Sentexa</span>
+          </div>
+
+          {/* Menu Links */}
+          <ul className="flex items-center space-x-8">
+            <li>
+              <a href="#fitur" className="hover:text-blue-500 transition">
+                Fitur
+              </a>
+            </li>
+            <li>
+              <Link
+                href="#cara-kerja"
+                className="hover:text-blue-500 transition"
+              >
+                Cara kerja
+              </Link>
+            </li>
+            <li>
+              <a href="#harga" className="hover:text-blue-500 transition">
+                Harga
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+
+        {/* Auth Buttons */}
+        <div className="flex items-center gap-4">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/login"
+            className="text-[#1E2939] border-[#6A7282] border px-6 py-2 rounded-lg font-semibold hover:text-blue-700"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            Masuk
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/register"
+            className="bg-[#1E2939] text-white font-semibold px-6 py-2 rounded-lg hover:bg-blue-700 transition"
           >
-            Documentation
+            Daftar Gratis
           </a>
         </div>
-      </main>
+      </nav>
     </div>
   );
 }
